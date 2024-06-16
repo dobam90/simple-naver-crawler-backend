57b3bd76c0dee6af14b5be24d9f5e98bfd62960e
Dobam <dobam@Dobamui-MacBookPro.local>
2024-06-16 18:40:17 +0900

    m

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ def check_blog_position(keyword, blog_id):
     logger.info(f"keyword : {keyword}")
     url = f"https://search.naver.com/search.naver?query={keyword}"
     response = requests.get(url)
+    
+    logger.info(f"response data: {json.dumps(response, indent=2)}")
     soup = BeautifulSoup(response.text, "html.parser")
 
     result = []
